82381eee6048a5d7eb550be7c73ff8d11b18f57b
Fakhri <fakhialtafian010@gmail.com>
2026-05-01 17:49:18 +0700

    CAP-70 (feat): add landing page, navbar, hero, footer, auth modal, and about us page

--- src/app/store/authStore.ts
+++ src/app/store/authStore.ts
@@ -5,15 +5,19 @@ interface User {
   id: string;
   email: string;
   name: string;
+  isModalOpen: boolean;
 }
 
 interface AuthState {
   token: string | null;
   role: string | null;
   user: User | null;
+  isModalOpen: boolean;
   
   setAuth: (token: string, role: string, user: User) => void;
   clearAuth: () => void;
+  openModal: () => void;
+  closeModal: () => void;
 }
 
 export const useAuthStore = create<AuthState>()(
@@ -21,15 +25,17 @@ export const useAuthStore = create<AuthState>()(
       token: null,
       role: null,
       user: null,
+      isModalOpen: false,
 
       setAuth: (token, role, user) => set({ token, role, user }),
-
       clearAuth: () => 
         set({ 
           token: null, 
           role: null, 
           user: null 
         }),
+        openModal: () => set({ isModalOpen: true }),
+        closeModal: () => set({ isModalOpen: false }),
     }),
     {
       name: 'auth-storage',
